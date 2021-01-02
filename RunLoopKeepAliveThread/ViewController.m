cae31d9937e1119e9931aab2d6f2ad8d132ee46a
Jinwei <renjwjx@gmail.com>
2021-01-02 22:15:48 +0800

    add keep alive thread

--- RunLoopKeepAliveThread/ViewController.m
+++ RunLoopKeepAliveThread/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) NSThread* thread;
 @end
 
 @implementation ViewController
@@ -19,5 +19,31 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)createThread:(id)sender {
+    NSThread* thread = [[NSThread alloc] initWithTarget:self selector:@selector(threadMain) object:nil];
+    [thread start];
+    self.thread = thread;
+    
+}
+
+- (void)threadMain
+{
+    NSLog(@"@thread start - %@", [NSThread currentThread]);
+    [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
+    //add port 会让current RunLoop 不会退出
+    [[NSRunLoop currentRunLoop] run];
+    
+}
+
+- (IBAction)performTask:(id)sender
+{
+    NSLog(@"start perfromTask");
+    [self performSelector:@selector(performTaskOnThread) onThread:self.thread withObject:nil waitUntilDone:YES];
+}
+
+- (void)performTaskOnThread
+{
+    NSLog(@"perfromTask on thread %@", [NSThread currentThread]);
+}
 
 @end
